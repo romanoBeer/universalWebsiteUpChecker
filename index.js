5bbed0b8d448eca777723e9d2d5072f5bc285d1f
romanoBeer <99826861+romanoBeer@users.noreply.github.com>
2023-09-08 14:04:50 +0200

    some functions

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-let sites = ["http://sadsadsa.co.za/", "https://n-8.co.za/", "https://romanobeer.github.io/", "https://mahindrafarmequipment.co.za/", "https://mahindrajhbsouth.co.za/", "https://ipopcars.co.za/", "https://pegc.co.za/", "https://xneelo.co.za/"];
-console.log(sites);
-
+let sites = ["https://mahindrafarmequipment.co.za/", "https://mitsubishimotorsumhlanga.co.za/home/", "https://nmidsmhavalumhlanga.co.za/",
+    "https://mahindrajhbsouth.co.za/", "https://ipopcars.co.za/", "https://pegc.co.za/", "https://xneelo.co.za/", "https://cherybloemfontein.co.za/",
+    "https://cherydurbancentral.co.za/", "https://cheryjhbsouth.co.za/", "https://cherykimberley.co.za/", "https://gwmmbombela.co.za/", "https://havalbloemfontein.co.za/",
+    "https://havalmbombela.co.za/", "https://havalpretorianorth.co.za/", "https://havaltheglen.co.za/", "https://havaltoti.co.za/", "https://islandwayvilla.co.za/",
+    "https://kleosscapital.com/", "https://mycherywelkom.co.za/", "https://nmichery.co.za/", "http://sacnaspevent.co.za/", "https://havalballito.co.za/",
+    "https://bayresurgence.co.za/", "https://havaltheglen.co.za/", "https://havaltoti.co.za/", "https://ipatc.joburg/"
+];
 let siteCount = sites.length;
 let up = 0,
     down = 0,
@@ -19,59 +23,6 @@ function openModal() {
 function closeModal() {
     var modal = document.getElementById("myModal");
     modal.style.display = "none";
-}
-
-//Display Custom Site Status
-siteName.addEventListener("keypress", (e) => {
-    if(e.key === "Enter"){
-        let customSite = [siteName.value];
-        console.log(customSite);
-        checkCustom(customSite);
-        checkCustomAvailability();
-        console.log(isValidUrl(customSite));
-
-        customSite.innerHTML += `<div style="color:#818181">${item}</div>`;
-    }else{
-        console.log("not enter");
-    }
-});
-
-//URL Validation
-const isValidUrl = urlString=> {
-    try { 
-        return Boolean(new URL(urlString)); 
-    }
-    catch(e){ 
-        return false; 
-    }
-}
-
-//Check custom website for up or down status
-function checkCustom(customSite) {
-    customSite.forEach(url => {
-        checkCustomAvailability(url);
-    });
-}
-
-function checkCustomAvailability(url) {
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                customDown++;
-                return;
-            } else {
-                customUp++;
-                return;
-            }
-        })
-        .catch(error => {
-            document.getElementById("customStatus").innerHTML = customDown;
-            return `${url} is down (Error: ${error.message}).`;
-        })
-        .then(result => {
-            const listItem = document.createElement("li");
-            listItem.textContent = result;
-        });
 }
 
 //Display all websites
@@ -114,4 +65,71 @@ function checkWebsiteAvailability(url) {
 
 checkAllWebsites();
 
+let displayCustom = document.getElementById("displayCustom");
 
+let customSite = [];
+
+//Display Custom Site Status
+siteName.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        pushData(customSite);
+        console.log(customSite);
+
+        if (isValidUrl(customSite) == true) {
+            checkCustom(customSite);
+            
+        } else {
+            console.log("this is not a valid site");
+        };
+    } else {
+        return;
+    };
+
+    document.getElementById("noCustomSites").style.display = "none";
+    customSite.map((item) => {
+        displayCustom.innerHTML += `<div style="color:#818181">${item}</div>`;
+    });
+});
+
+function pushData(customSite) {
+
+    const data = document.getElementById('searchTerm').value;
+    customSite.push(data);
+}
+
+
+//URL Validation
+const isValidUrl = urlString => {
+    try {
+        return Boolean(new URL(urlString));
+    } catch (e) {
+        return false;
+    }
+}
+
+//Check custom website for up or down status
+function checkCustom(customSite) {
+    customSite.forEach(url => {
+        checkCustomAvailability(url);
+    });
+}
+
+function checkCustomAvailability(url) {
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                customDown++;
+            } else {
+                customUp++;
+                document.getElementById("customUp").innerHTML = customUp;
+            }
+        })
+        .catch(error => {
+            document.getElementById("customDown").innerHTML = customDown;
+            return;
+        })
+        .then(result => {
+            const listItem = document.createElement("li");
+            listItem.textContent = result;
+        });
+}
